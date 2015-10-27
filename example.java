0d183c187152de154df48443a0c983ac05b0f561
Ares Yang <yyh.fanatic@gmail.com>
2015-10-26 23:01:17 -0700

    leetcode

---
+++ example.java
@@ -0,0 +1,9 @@
+/*
+	This is example in java, test java compile function in terminal 
+*/
+class Example {
+	// program begin with a call to main
+	public static void main(String args []) {
+		System.out.println("This is a simple Java program");
+	}
+}
