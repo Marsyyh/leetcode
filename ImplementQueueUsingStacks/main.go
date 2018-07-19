fd93ff4f9807f68803cfdab35ee97d49c48dc213
Ares <Ares.Yang@wbconsultant.com>
2018-07-19 11:49:30 -0700

    move datastructure to another project

--- ImplementQueueUsingStacks/main.go
+++ ImplementQueueUsingStacks/main.go
@@ -1,52 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type (
-	Stack struct {
-		top    *node
-		length int
-	}
-	node struct {
-		value interface{}
-		next  *node
-	}
+	"github.com/Marsyyh/go-datastructure/container/stack"
 )
-
-func New() *Stack {
-	return &Stack{nil, 0}
-}
-
-func (s *Stack) Push(value interface{}) {
-	n := &node{value, s.top}
-	s.top = n
-	s.length++
-}
-
-func (s *Stack) Pop() interface{} {
-	if s.length == 0 {
-		return nil
-	}
-	value := s.top.value
-	s.top = s.top.next
-	s.length--
-	return value
-}
-
-func (s *Stack) Peek() interface{} {
-	if s.length == 0 {
-		return nil
-	}
-	return s.top.value
-}
-
-func (s *Stack) Len() int {
-	return s.length
-}
-
-func (s *Stack) IsEmpty() bool {
-	return s.top == nil
-}
 
 type MyQueue struct {
 	value int
@@ -58,7 +16,7 @@ func MyQueueBuilder(value int, test []int) MyQueue {
 }
 
 func main() {
-	s := New()
+	s := stack.New()
 	s.Push(20)
 	s.Push(50)
 	fmt.Println(s)
