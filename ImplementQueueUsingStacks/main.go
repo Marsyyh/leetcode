2269a8d1df25bc28a2c910a588d970d296ffd0c2
Ares <Ares.Yang@wbconsultant.com>
2018-07-20 17:57:13 -0700

    add

--- ImplementQueueUsingStacks/main.go
+++ ImplementQueueUsingStacks/main.go
@@ -6,27 +6,69 @@ import (
 	"github.com/Marsyyh/go-datastructure/container/stack"
 )
 
+// MyQueue is the queue implemented with two stack
 type MyQueue struct {
-	value int
-	test  []int
+	inStack  stack.Stack
+	outStack stack.Stack
+	length   int
 }
 
-func MyQueueBuilder(value int, test []int) MyQueue {
-	return MyQueue{value, test}
+// Constructor to creat MyQueue
+func Constructor() MyQueue {
+	return MyQueue{stack.NewLinkedListStack(), stack.NewLinkedListStack(), 0}
+}
+
+// Push element x to the back of queue.
+func (q *MyQueue) Push(x int) {
+	q.inStack.Push(x)
+	q.length++
+}
+
+// Pop the element from in front of queue and returns that element.
+func (q *MyQueue) Pop() int {
+	if !q.adjust() {
+		return -1
+	}
+	q.length--
+	if num, ok := q.outStack.Pop().(int); ok {
+		return num
+	}
+	return -1
+}
+
+// Peek the front element. */
+func (q *MyQueue) Peek() int {
+	if !q.adjust() {
+		return -1
+	}
+	q.adjust()
+	if num, ok := q.outStack.Peek().(int); ok {
+		return num
+	}
+	return -1
+}
+
+// Empty returns whether the queue is empty. */
+func (q *MyQueue) Empty() bool {
+	return q.length == 0
+}
+
+func (q *MyQueue) adjust() bool {
+	if q.length == 0 {
+		return false
+	}
+	if q.outStack.IsEmpty() {
+		for q.inStack.Peek() != nil {
+			q.outStack.Push(q.inStack.Pop())
+		}
+	}
+	return true
 }
 
 func main() {
-	s := stack.New()
-	s.Push(20)
-	s.Push(50)
-	fmt.Println(s)
-	fmt.Println(s.Len())
-	fmt.Println(s.Peek())
-	fmt.Println(s.Pop())
-	fmt.Println(s.Peek())
-	fmt.Println(s.Len())
-	fmt.Println(s.IsEmpty())
-	s.Pop()
-	fmt.Println(s.IsEmpty())
-	fmt.Println(s.IsEmpty())
+	obj := Constructor()
+	obj.Push(1)
+	fmt.Println(obj.Pop())
+	fmt.Println(obj.Peek())
+	fmt.Println(obj.Empty())
 }
