1eb1eac649dea65a6b29bacf52e656d67ec49bf8
ares <yanhong.yang@allride.ai>
2021-05-17 19:35:21 +0800

    add leetcode

--- 43.字符串相乘.cpp
+++ 43.字符串相乘.cpp
@@ -28,13 +28,22 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        
+        string result = "0";
+
         if (num1.size() == 0 || num2.size() == 0) {
-            return string("0");
+            return result;
         }
         
         for (int i = 0; i < num1.size(); i++) {
-            
+            string cur = "";
+            char nextBit = '0';
+            int num1Bit = num1[i] - '0';
+            for (int j = 0; j < num2.size(); j++) {
+                int num2Bit = num2[j] - '0';
+                int curRe = num1Bit * num2Bit + (nextBit - '0');
+                nextBit = (curRe / 10) + '0';
+                cur.append((curRe % 10)+'0');
+            }
         }
     }
 };
